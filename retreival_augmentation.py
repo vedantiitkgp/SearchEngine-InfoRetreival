a263fbbdc0baa205bcfab077614990a0d8e5bde7
vedantiitkgp <vedantntpc@gmail.com>
2024-03-17 16:49:53 -0700

    minor chnages

--- retreival_augmentation.py
+++ retreival_augmentation.py
@@ -1,30 +1,27 @@
 import json
+import os
 import gzip
 import math
 import time
-import numpy as np
-#import tkinter as tk
 from tkinter import ttk as tk
 from tkinter import *
-from tkinter.messagebox import askyesno 
-from numpy.linalg import norm
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from collections import defaultdict
-from sklearn.metrics.pairwise import cosine_similarity
 import networkx as nx
 
 class RetreivalAugmentation:
-    def __init__(self, threshold):
+    def __init__(self, tfidf_folder, neighbors_folder):
+        self.tfidf_folder = tfidf_folder
+        self.neighbors_folder = neighbors_folder
         self.ps = PorterStemmer()
-        self.inverted_index = {}
-        self.pagerank = {}
+        self.pagerank = defaultdict(dict)
         self.query = ''
-        self.tfidf_table  = {}
+        self.tfidf_table  = defaultdict(dict)
         self.query_token_docs = set()
-        self.inverted_index_dict= {}
-        self.pagerank_scores = {}
+        self.pagerank_scores = defaultdict(dict)
         self.results = []
+        self.G = nx.Graph() 
         
 
 
@@ -48,19 +45,16 @@ class RetreivalAugmentation:
         
         query_tfidf_relevance_score = defaultdict(float)
 
-
-
-
         #Cosine similarity calculations
         for term in query_token_count:
-            for url in self.inverted_index[term]:
+            for url in self.tfidf_table[term]:
                 tfidf = self.tfidf_table[term][url]  
                 tfidf_score = query_idf_weights[term] * tfidf   
                 query_tfidf_relevance_score[url] += tfidf_score
         
         # Calculate relevance score based on cosine similarity and pagerank
         for url, score in query_tfidf_relevance_score.items():
-            query_tfidf_relevance_score[url] = 0.8*score + 0.2*self.pagerank[url] 
+            query_tfidf_relevance_score[url] = 0.8*score +10*self.pagerank.get(url,0) 
 
         sorted_query_tfidf_relevance_score = dict(sorted(query_tfidf_relevance_score.items(), key=lambda item: item[1]))
         top_results = list(sorted_query_tfidf_relevance_score.keys())[-5:]
@@ -76,85 +70,79 @@ class RetreivalAugmentation:
     def query_call(self, inp): 
         self.query = inp
         self.results = self.start()
-        # while True:
-        #   permission = input("Do you want to run another query: (y/n) ")
-        #   if permission == "y":
-        #     query = input("Enter your new Query: ")
-        #     print("--- Processing this query ---")
-        #     self.query = query
-        #     self.results = self.start()
-        #   else:
-        #     break
-          
-    def calculate_page_rank(self):
-        url_graph = nx.DiGraph()
-        for token , url_dict in self.inverted_index.items():     
-            for url, count in url_dict.items():
-                url_graph.add_node(url)
-            url_graph.add_edges_from([(url, terms[0]) for terms in url_dict])
+        print(self.results)
 
-        self.pageScores = nx.pagerank(url_graph)
-        return 
     
     def show_results(self):
         root1 = Tk()
-        root1.geometry('500x700') 
+        root1.geometry('300x600') 
         frm = tk.Frame(root1, padding=10)
         frm.grid()
-        tk.Label(frm, text=self.results[0], justify = CENTER).grid(column=0, row=0)
+        tk.Label(frm, text=self.results[0]).grid(column=0, row=0)
         tk.Label(frm, text=self.results[1]).grid(column=0, row=1)
         tk.Label(frm, text=self.results[2]).grid(column=0, row=2)
         tk.Label(frm, text=self.results[3]).grid(column=0, row=3)
         tk.Label(frm, text=self.results[4]).grid(column=0, row=4)
         tk.Button(frm, text="Quit", command=root1.destroy).grid(column=0, row=7)
         root1.mainloop()
-        
+
         
     def initate_call(self):
-        print('--- Reading PageRank, Inverted Index and TfIdf Json ---- ')
-               
-        with gzip.open('compressed_pagerank.json.gz', 'rb') as pagerank_file:
-            compressed_pagerank_file = pagerank_file.read()
-            decompressed_pagerank_file = gzip.decompress(compressed_pagerank_file)
-            self.pagerank = json.loads(decompressed_pagerank_file.decode('utf-8'))
+        print('---  Reading TfIdf Json ---- ')
         
-        with gzip.open('compressed_inverted_index.json.gz', 'rb') as inverted_index_file:
-            compressed_inverted_index_file = inverted_index_file.read()
-            decompressed_inverted_index_file = gzip.decompress(compressed_inverted_index_file)
-            self.inverted_index = json.loads(decompressed_inverted_index_file.decode('utf-8'))
-            self.inverted_index_dict = {key: index for index, key in enumerate(self.inverted_index.keys())}
-            self.tokens = self.inverted_index_dict.keys()
+        for filename in os.listdir(self.tfidf_folder):
+            if filename.endswith('.gz'):
+                print(filename)
+                file_path = os.path.join(self.tfidf_folder, filename)
+                with gzip.open(file_path, 'rb') as tfidf_file:
+                    compressed_tfidf_file = tfidf_file.read()
+                    decompressed_tfidf_file = gzip.decompress(compressed_tfidf_file) 
+                    partial_tfidf_index = json.loads(decompressed_tfidf_file.decode('utf-8'))
+                    for term, postings in partial_tfidf_index.items():
+                        for url in postings:
+                            self.tfidf_table[term][url] = postings[url]
+        self.tokens = self.tfidf_table.keys()
 
-        with gzip.open('compressed_tfidf.json.gz', 'rb') as tfidf_file:
-            compressed_tfidf_file = tfidf_file.read()
-            decompressed_tfidf_file = gzip.decompress(compressed_tfidf_file) 
-            self.tfidf_table = json.loads(decompressed_tfidf_file.decode('utf-8'))
+        for filename in os.listdir(self.neighbors_folder):
+            if filename.startswith('neighbors') and filename.endswith('.gz'):
+                file_path = os.path.join(self.neighbors_folder, filename)
+                with gzip.open(file_path, 'rb') as neighbors_file:
+                    compressed_neighbors_file = neighbors_file.read()
+                    decompressed_neighbors_file = gzip.decompress(compressed_neighbors_file) 
+                    partial_neighbors_index = json.loads(decompressed_neighbors_file.decode('utf-8'))
+                    for url in partial_neighbors_index:
+                        for link in partial_neighbors_index[url]:
+                            self.G.add_edge(url,link)
+
+        self.pagerank = nx.pagerank(self.G)
 
         def search_query():
-            inp = entry.get()  # Get the search query from the entry widget
+            query = input("Enter your Query: ")
+            # inp = entry.get()  # Get the search query from the entry widget
             # Perform the search (replace this with your actual search function)
-            self.query_call(inp)
-            self.show_results()
+            self.query_call(query)
+            # self.show_results()
 
-        root = Tk()
-        root.geometry('500x700') 
-        entry = tk.Entry(root, textvariable = 'Search Engine', justify = CENTER) 
+        search_query()
+        # root = Tk()
+        # root.geometry('300x600') 
+        # entry = tk.Entry(root, textvariable = 'Search Engine', justify = CENTER) 
   
         # focus_force is used to take focus 
         # as soon as application starts 
-        entry.focus_force() 
-        entry.pack(side = TOP, ipadx = 30, ipady = 6) 
+        # entry.focus_force() 
+        # entry.pack(side = TOP, ipadx = 30, ipady = 6) 
         
-        search = tk.Button(root, text = 'Search', command = lambda : search_query()) 
-        search.pack(side = TOP, pady = 10) 
+        # search = tk.Button(root, text = 'Search', command = lambda : search_query()) 
+        # search.pack(side = TOP, pady = 10) 
         
-        root.mainloop()
+        # root.mainloop()
                               
         #GUI partially taken from https://www.geeksforgeeks.org/how-to-get-the-input-from-tkinter-text-box/
         
         
 if __name__ == "__main__":
-    rag = RetreivalAugmentation(200)
+    rag = RetreivalAugmentation('tfidf','index')
     rag.initate_call()
 
 
